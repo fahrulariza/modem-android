b4187a06d48ddfad7c5f35223afc6f7c6bd2c7b7
fahrulariza <131193627+fahrulariza@users.noreply.github.com>
2024-09-23 01:11:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,86 +1,22 @@
 #!/bin/bash
 
-# Base URL for raw GitHub content
-BASE_URL="https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master"
+ping_url=${1:-"8.8.8.8"}
+sdcard_path="/sdcard/modem.txt"
+echo "$ping_url" > "$sdcard_path"
 
-# Create necessary directories
-mkdir -p META-INF/com/google/android
-mkdir -p system/bin
-
-# Download files
-echo "Downloading necessary files..."
-
-# Download META-INF files
-curl -L -o META-INF/com/google/android/update-binary "$BASE_URL/META-INF/com/google/android/update-binary"
-curl -L -o META-INF/com/google/android/updater-script "$BASE_URL/META-INF/com/google/android/updater-script"
-
-# Download system/bin files
-curl -L -o system/bin/module_ping.sh "$BASE_URL/system/bin/module_ping.sh"
-curl -L -o system/bin/module-adbau.sh "$BASE_URL/system/bin/module-adbau.sh"
-curl -L -o system/bin/module-batt.sh "$BASE_URL/system/bin/module-batt.sh"
-
-# Download other necessary files
-curl -L -o install.bat "$BASE_URL/install.bat"
-curl -L -o module.prop "$BASE_URL/module.prop"
-
-# Check if all files were downloaded successfully
-if [ -f META-INF/com/google/android/update-binary ] && [ -f META-INF/com/google/android/updater-script ] && \
-   [ -f system/bin/module_ping.sh ] && [ -f system/bin/module-adbau.sh ] && [ -f system/bin/module-batt.sh ] && \
-   [ -f install.bat ] && [ -f module.prop ]; then
-  echo "All files downloaded successfully!"
-else
-  echo "Error: Some files could not be downloaded. Please check your internet connection or the GitHub repository."
-  exit 1
-fi
-
-# Zip the folder to create Magisk module
-echo "Creating MagiskModemPing.zip..."
-zip -r MagiskModemPing.zip META-INF system install.sh install.bat module.prop
-
-# Function to move the zip file to the Downloads folder based on OS
-move_to_downloads() {
-  OS=$(uname -s)  # Detect the operating system
-  case $OS in
-    Linux)
-      if [ -d "$HOME/Downloads" ]; then
-        mv "MagiskModemPing.zip" "$HOME/Downloads/"
-        echo "MagiskModemPing.zip moved to $HOME/Downloads"
-      else
-        echo "Downloads folder not found in Linux."
-      fi
-      ;;
-    Darwin)  # For macOS
-      if [ -d "$HOME/Downloads" ]; then
-        mv "MagiskModemPing.zip" "$HOME/Downloads/"
-        echo "MagiskModemPing.zip moved to $HOME/Downloads"
-      else
-        echo "Downloads folder not found in macOS."
-      fi
-      ;;
-    CYGWIN*|MINGW*|MSYS*)  # For Windows
-      DOWNLOAD_DIR="$(cygpath -w "$USERPROFILE\\Downloads")"
-      if [ -d "$DOWNLOAD_DIR" ]; then
-        mv "MagiskModemPing.zip" "$DOWNLOAD_DIR/"
-        echo "MagiskModemPing.zip moved to $DOWNLOAD_DIR"
-      else
-        echo "Downloads folder not found in Windows."
-      fi
-      ;;
-    Android)
-      if [ -d "/sdcard/Download" ]; then
-        mv "MagiskModemPing.zip" "/sdcard/Download/"
-        echo "MagiskModemPing.zip moved to /sdcard/Download"
-      else
-        echo "Downloads folder not found on Android."
-      fi
-      ;;
-    *)
-      echo "Unknown OS. Zip file will be in the current directory."
-      ;;
-  esac
+download_file() {
+  url=$1
+  path=$2
+  curl -L "$url" -o "$path"
 }
 
-# Move the zip file to the appropriate Downloads folder
-move_to_downloads
+mkdir -p MagiskModemPing/META-INF/com/google/android/
+mkdir -p MagiskModemPing/system/bin/
 
-echo "Magisk module compilation completed."
+# Download necessary files
+download_file "https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/META-INF/com/google/android/update-binary" "MagiskModemPing/META-INF/com/google/android/update-binary"
+download_file "https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/META-INF/com/google/android/updater-script" "MagiskModemPing/META-INF/com/google/android/updater-script"
+download_file "https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module_ping.sh" "MagiskModemPing/system/bin/module_ping.sh"
+download_file "https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module-adbau.sh" "MagiskModemPing/system/bin/module-adbau.sh"
+download_file "https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module-batt.sh" "MagiskModemPing/system/bin/module-batt.sh"
+download_file "https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/module.prop" "MagiskModemPing/module.prop"
