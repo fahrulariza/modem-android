afcb8497d372c2f15b3199624f86e25b631c4fd1
fahrulariza <131193627+fahrulariza@users.noreply.github.com>
2024-09-23 02:19:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
+
 # Memastikan direktori modem-android ada
 mkdir -p ~/modem-android
 cd ~/modem-android
+
 # URL Ping yang diset melalui argumen
 URL_PING=${1:-"8.8.8.8"}
+
 # Menulis URL ke file modem.txt
-echo "$URL_PING" > -p ~/modem-android/modem.txt
+echo "$URL_PING" > /sdcard/modem-android/modem.txt
+
 # Mengunduh file yang diperlukan
 echo "Mengunduh file untuk Magisk module..."
-wget -P ~/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/META-INF/com/google/android/update-binary
-wget -P ~/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/META-INF/com/google/android/updater-script
-wget -P ~/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module_ping.sh
-wget -P ~/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module-adbau.sh
-wget -P ~/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module-batt.sh
-wget -P ~/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/install.sh
-wget -P ~/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/module.prop
-wget -P ~/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/readme.md
+FILES=(
+    "META-INF/com/google/android/update-binary"
+    "META-INF/com/google/android/updater-script"
+    "system/bin/module_ping.sh"
+    "system/bin/module-adbau.sh"
+    "system/bin/module-batt.sh"
+    "install.sh"
+    "module.prop"
+    "readme.md"
+)
+
+for FILE in "${FILES[@]}"; do
+    if wget -P /sdcard/modem-android "https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/$FILE"; then
+        echo "Successfully downloaded $FILE"
+    else
+        echo "Failed to download $FILE"
+    fi
+done
+
 # Kompres folder menjadi zip
-cd /sdcard
+cd /sdcard || { echo "Failed to change directory to /sdcard"; exit 1; }
 zip -r modem-android.zip modem-android/
 echo "Proses selesai. Module tersimpan di /sdcard/modem-android.zip"
