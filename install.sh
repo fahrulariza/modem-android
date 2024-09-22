6a1667a27005d82c64896b16172c2a00e47f79ab
fahrulariza <131193627+fahrulariza@users.noreply.github.com>
2024-09-23 02:36:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Memastikan direktori modem-android ada
-mkdir -p ~/modem-android/META-INF/com/google/android
-mkdir -p ~/modem-android/system/bin
+# Memastikan direktori modem-android dan subdirektorinya ada
+mkdir -p /storage/emulated/0/modem-android/META-INF/com/google/android
+mkdir -p /storage/emulated/0/modem-android/system/bin
 
 # URL Ping yang diset melalui argumen
 URL_PING=${1:-"8.8.8.8"}
@@ -12,21 +12,24 @@ echo "$URL_PING" > /storage/emulated/0/modem-android/modem.txt
 
 # Mengunduh file yang diperlukan
 echo "Mengunduh file untuk Magisk module..."
-FILES=(
-    "META-INF/com/google/android/update-binary"
-    "META-INF/com/google/android/updater-script"
-    "system/bin/module_ping.sh"
-    "system/bin/module-adbau.sh"
-    "system/bin/module-batt.sh"
-    "install.sh"
-    "module.prop"
-    "README.md"
+
+# Daftar file dan path tujuan
+declare -A FILES=(
+    ["META-INF/com/google/android/update-binary"]="/storage/emulated/0/modem-android/META-INF/com/google/android/update-binary"
+    ["META-INF/com/google/android/updater-script"]="/storage/emulated/0/modem-android/META-INF/com/google/android/updater-script"
+    ["system/bin/module_ping.sh"]="/storage/emulated/0/modem-android/system/bin/module_ping.sh"
+    ["system/bin/module-adbau.sh"]="/storage/emulated/0/modem-android/system/bin/module-adbau.sh"
+    ["system/bin/module-batt.sh"]="/storage/emulated/0/modem-android/system/bin/module-batt.sh"
+    ["install.sh"]="/storage/emulated/0/modem-android/install.sh"
+    ["module.prop"]="/storage/emulated/0/modem-android/module.prop"
+    ["README.md"]="/storage/emulated/0/modem-android/README.md"
 )
 
 BASE_URL="https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/"
 
-for FILE in "${FILES[@]}"; do
-    if wget -P /storage/emulated/0/modem-android "$BASE_URL$FILE"; then
+# Mengunduh setiap file ke lokasi yang sesuai
+for FILE in "${!FILES[@]}"; do
+    if wget -O "${FILES[$FILE]}" "$BASE_URL$FILE"; then
         echo "Successfully downloaded $FILE"
     else
         echo "Failed to download $FILE"
