3a38b46c5c4cc54a9c4034605d8d6cd1f776ce37
fahrulariza <131193627+fahrulariza@users.noreply.github.com>
2024-09-23 02:41:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
-# Memastikan direktori modem-android dan subdirektorinya ada
-mkdir -p /storage/emulated/0/modem-android/META-INF/com/google/android
-mkdir -p /storage/emulated/0/modem-android/system/bin
+# Memastikan direktori modem-android ada
+BASE_DIR="/storage/emulated/0/modem-android"
+mkdir -p "$BASE_DIR/META-INF/com/google/android"
+mkdir -p "$BASE_DIR/system/bin"
 
 # URL Ping yang diset melalui argumen
 URL_PING=${1:-"8.8.8.8"}
 
 # Menulis URL ke file modem.txt
-echo "$URL_PING" > /storage/emulated/0/modem-android/modem.txt
+echo "$URL_PING" > "$BASE_DIR/modem.txt"
 
 # Mengunduh file yang diperlukan
 echo "Mengunduh file untuk Magisk module..."
 
 # Daftar file dan path tujuan
-declare -A FILES=(
-    ["META-INF/com/google/android/update-binary"]="/storage/emulated/0/modem-android/META-INF/com/google/android/update-binary"
-    ["META-INF/com/google/android/updater-script"]="/storage/emulated/0/modem-android/META-INF/com/google/android/updater-script"
-    ["system/bin/module_ping.sh"]="/storage/emulated/0/modem-android/system/bin/module_ping.sh"
-    ["system/bin/module-adbau.sh"]="/storage/emulated/0/modem-android/system/bin/module-adbau.sh"
-    ["system/bin/module-batt.sh"]="/storage/emulated/0/modem-android/system/bin/module-batt.sh"
-    ["install.sh"]="/storage/emulated/0/modem-android/install.sh"
-    ["module.prop"]="/storage/emulated/0/modem-android/module.prop"
-    ["README.md"]="/storage/emulated/0/modem-android/README.md"
+FILES=(
+    "META-INF/com/google/android/update-binary"
+    "META-INF/com/google/android/updater-script"
+    "system/bin/module_ping.sh"
+    "system/bin/module-adbau.sh"
+    "system/bin/module-batt.sh"
+    "install.sh"
+    "module.prop"
+    "README.md"
 )
 
 BASE_URL="https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/"
 
 # Mengunduh setiap file ke lokasi yang sesuai
-for FILE in "${!FILES[@]}"; do
-    if wget -O "${FILES[$FILE]}" "$BASE_URL$FILE"; then
+for FILE in "${FILES[@]}"; do
+    TARGET_PATH="$BASE_DIR/$FILE"
+    
+    # Membuat direktori jika perlu
+    mkdir -p "$(dirname "$TARGET_PATH")"
+    
+    # Mengunduh file
+    if wget -O "$TARGET_PATH" "$BASE_URL$FILE"; then
         echo "Successfully downloaded $FILE"
     else
         echo "Failed to download $FILE"
