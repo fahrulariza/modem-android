7de664ee501969110d4e2febc341669a78fdfd3d
fahrulariza <131193627+fahrulariza@users.noreply.github.com>
2024-09-23 01:52:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
 # Memastikan direktori modem-android ada
 mkdir -p /storage/emulated/0/modem-android
-
 # URL Ping yang diset melalui argumen
 URL_PING=${1:-"8.8.8.8"}
-
 # Menulis URL ke file modem.txt
 echo "$URL_PING" > /sdcard/modem-android/modem.txt
-
 # Mengunduh file yang diperlukan
 echo "Mengunduh file untuk Magisk module..."
 wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/META-INF/com/google/android/update-binary
@@ -19,9 +15,7 @@ wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/mode
 wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/install.sh
 wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/module.prop
 wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/readme.md
-
 # Kompres folder menjadi zip
 cd /sdcard
 zip -r modem-android.zip modem-android/
-
 echo "Proses selesai. Module tersimpan di /sdcard/modem-android.zip"
