165911f20659ea785a4560befcbd9049e47f8e83
fahrulariza <131193627+fahrulariza@users.noreply.github.com>
2024-09-23 02:32:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Memastikan direktori modem-android ada
-mkdir -p ~/modem-android
-cd ~/modem-android
+mkdir -p ~/modem-android/META-INF/com/google/android
+mkdir -p ~/modem-android/system/bin
 
 # URL Ping yang diset melalui argumen
 URL_PING=${1:-"8.8.8.8"}
 
 # Menulis URL ke file modem.txt
-echo "$URL_PING" > /sdcard/modem-android/modem.txt
+echo "$URL_PING" > /storage/emulated/0/modem-android/modem.txt
 
 # Mengunduh file yang diperlukan
 echo "Mengunduh file untuk Magisk module..."
@@ -23,8 +23,10 @@ FILES=(
     "README.md"
 )
 
+BASE_URL="https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/"
+
 for FILE in "${FILES[@]}"; do
-    if wget -P /sdcard/modem-android "https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/$FILE"; then
+    if wget -P /storage/emulated/0/modem-android "$BASE_URL$FILE"; then
         echo "Successfully downloaded $FILE"
     else
         echo "Failed to download $FILE"
@@ -32,6 +34,6 @@ for FILE in "${FILES[@]}"; do
 done
 
 # Kompres folder menjadi zip
-cd /sdcard || { echo "Failed to change directory to /sdcard"; exit 1; }
+cd /storage/emulated/0 || { echo "Failed to change directory to /storage/emulated/0"; exit 1; }
 zip -r modem-android.zip modem-android/
-echo "Proses selesai. Module tersimpan di /sdcard/modem-android.zip"
+echo "Proses selesai. Module tersimpan di /storage/emulated/0/modem-android.zip"
