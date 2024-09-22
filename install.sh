9fc2fc21bda46c42b5cac9e535568171ae4c232c
fahrulariza <131193627+fahrulariza@users.noreply.github.com>
2024-09-23 01:36:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-URL=${1:-"8.8.8.8"}
-echo $URL > /sdcard/modem.txt
-echo "URL Ping telah diatur ke $URL"
+# Memastikan direktori modem-android ada
+mkdir -p /sdcard/modem-android
 
-mkdir -p /storage/emulated/0/modem-android
-wget https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/META-INF/com/google/android/update-binary -O /storage/emulated/0/modem-android/update-binary
-wget https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/META-INF/com/google/android/updater-script -O /storage/emulated/0/modem-android/updater-script
-wget https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module_ping.sh -O /storage/emulated/0/modem-android/module_ping.sh
-wget https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module-adbau.sh -O /storage/emulated/0/modem-android/module-adbau.sh
-wget https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module-batt.sh -O /storage/emulated/0/modem-android/module-batt.sh
-wget https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/module.prop -O /storage/emulated/0/modem-android/module.prop
-wget https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/readme.md -O /storage/emulated/0/modem-android/readme.md
+# URL Ping yang diset melalui argumen
+URL_PING=${1:-"8.8.8.8"}
 
-cd /storage/emulated/0
+# Menulis URL ke file modem.txt
+echo "$URL_PING" > /sdcard/modem-android/modem.txt
+
+# Mengunduh file yang diperlukan
+echo "Mengunduh file untuk Magisk module..."
+wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/META-INF/com/google/android/update-binary
+wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/META-INF/com/google/android/updater-script
+wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module_ping.sh
+wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module-adbau.sh
+wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/system/bin/module-batt.sh
+wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/install.sh
+wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/module.prop
+wget -P /sdcard/modem-android https://raw.githubusercontent.com/fahrulariza/modem-android/refs/heads/master/readme.md
+
+# Kompres folder menjadi zip
+cd /sdcard
 zip -r modem-android.zip modem-android/
-echo "Magisk Module telah dibuat sebagai modem-android.zip di sdcard"
+
+echo "Proses selesai. Module tersimpan di /sdcard/modem-android.zip"
